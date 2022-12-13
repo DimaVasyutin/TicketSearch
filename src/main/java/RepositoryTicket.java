b7533b8dc0f8507cdb58f525717ec88de3353124
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-12-13 13:36:10 +0300

    Update RepositoryTicket.java

--- src/main/java/RepositoryTicket.java
+++ src/main/java/RepositoryTicket.java
@@ -1,16 +1,16 @@
 public class RepositoryTicket {
     Ticket[] tickets = new Ticket[0];
 
-    public Ticket[] allTicket(){
+    public Ticket[] allTicket() {
         return tickets;
     }
 
-    public void addTicket(Ticket ticket){
-        Ticket[] tmp = new Ticket[tickets.length +1];
-        for (int i=0; i< tickets.length; i++){
-            tmp[i]=tickets[i];
+    public void addTicket(Ticket ticket) {
+        Ticket[] tmp = new Ticket[tickets.length + 1];
+        for (int i = 0; i < tickets.length; i++) {
+            tmp[i] = tickets[i];
         }
-        tmp[tmp.length-1] = ticket;
+        tmp[tmp.length - 1] = ticket;
         tickets = tmp;
     }
 
