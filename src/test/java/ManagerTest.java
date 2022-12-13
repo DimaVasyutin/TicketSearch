a2eb664769f4c5d81cff43ddc2c59e1559d70619
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-12-13 13:36:42 +0300

    Update ManagerTest.java

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -5,39 +5,39 @@ public class ManagerTest {
     RepositoryTicket repo = new RepositoryTicket();
     ManagerTicket manager = new ManagerTicket(repo);
 
-    Ticket ticket1 = new Ticket(1, 30_000,"ATH", "ASF", 240);
-    Ticket ticket2 = new Ticket(2, 45_000,"EGO", "BJV", 300);
-    Ticket ticket3 = new Ticket(3, 20_000,"ATH", "ASF", 120);
-    Ticket ticket4 = new Ticket(4, 15_000,"LCA", "SQQ", 100);
+    Ticket ticket1 = new Ticket(1, 30_000, "ATH", "ASF", 240);
+    Ticket ticket2 = new Ticket(2, 45_000, "EGO", "BJV", 300);
+    Ticket ticket3 = new Ticket(3, 20_000, "ATH", "ASF", 120);
+    Ticket ticket4 = new Ticket(4, 15_000, "LCA", "SQQ", 100);
 
     @Test
-    public void testFindAll(){
+    public void testFindAll() {
         manager.addTicket(ticket1);
         manager.addTicket(ticket2);
         manager.addTicket(ticket3);
         manager.addTicket(ticket4);
 
         Ticket[] expected = {ticket3, ticket1};
-        Ticket[] actual = manager.findAll("ATH","ASF");
+        Ticket[] actual = manager.findAll("ATH", "ASF");
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testNotFindAll(){
+    public void testNotFindAll() {
         manager.addTicket(ticket1);
         manager.addTicket(ticket2);
         manager.addTicket(ticket3);
         manager.addTicket(ticket4);
 
         Ticket[] expected = new Ticket[0];
-        Ticket[] actual = manager.findAll("ATH","BJV");
+        Ticket[] actual = manager.findAll("ATH", "BJV");
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testRemoveById(){
+    public void testRemoveById() {
         manager.addTicket(ticket1);
         manager.addTicket(ticket2);
         manager.addTicket(ticket3);
@@ -50,26 +50,26 @@ public class ManagerTest {
     }
 
     @Test
-    public void testRemoveByIdNoId(){
+    public void testRemoveByIdNoId() {
         manager.addTicket(ticket1);
         manager.addTicket(ticket2);
         manager.addTicket(ticket3);
         manager.addTicket(ticket4);
 
         Ticket[] expected = new Ticket[0];
-        Ticket[] actual = manager.removeById(5);;
+        Ticket[] actual = manager.removeById(5);
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testFindById(){
+    public void testFindById() {
         manager.addTicket(ticket1);
         manager.addTicket(ticket2);
         manager.addTicket(ticket3);
         manager.addTicket(ticket4);
 
-        Ticket[] expected = { ticket2};
+        Ticket[] expected = {ticket2};
         Ticket[] actual = manager.findById(2);
 
         Assertions.assertArrayEquals(expected, actual);
