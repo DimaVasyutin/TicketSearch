9c059a820716dd467d62ee67bf680833ec539bc5
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-12-13 13:35:53 +0300

    Update ManagerTicket.java

--- src/main/java/ManagerTicket.java
+++ src/main/java/ManagerTicket.java
@@ -4,35 +4,35 @@ public class ManagerTicket {
 
     private RepositoryTicket repo;
 
-    public ManagerTicket(RepositoryTicket repo){
+    public ManagerTicket(RepositoryTicket repo) {
         this.repo = repo;
     }
 
-    public void addTicket(Ticket ticket){
+    public void addTicket(Ticket ticket) {
         repo.addTicket(ticket);
     }
 
-    public Ticket[] findById(int id){
+    public Ticket[] findById(int id) {
         Ticket[] tmp = repo.findById(id);
         return tmp;
     }
 
-    public Ticket[] removeById(int id){
+    public Ticket[] removeById(int id) {
         Ticket[] tmp = repo.removeById(id);
         return tmp;
     }
 
-    public Ticket[] allTicket(){
+    public Ticket[] allTicket() {
         Ticket[] tmp = repo.allTicket();
         return tmp;
     }
 
-    public Ticket[] findAll(String departure, String arrival){
+    public Ticket[] findAll(String departure, String arrival) {
         Ticket[] result = new Ticket[0];
         RepositoryTicket tmp = new RepositoryTicket();
 
-        for (Ticket ticket: allTicket()){
-            if (ticket.getDeparture() == departure && ticket.getArrival() == arrival){
+        for (Ticket ticket : allTicket()) {
+            if (ticket.getDeparture() == departure && ticket.getArrival() == arrival) {
                 tmp.addTicket(ticket);
                 result = tmp.allTicket();
             }
